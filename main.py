0913cafdc890faf6d7b0b5e19224d22e43be86d3
Evelīna Geikina <118215390+evelina1701@users.noreply.github.com>
2023-03-15 16:34:40 +0000

    test

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
-    #tiek izveidots masīvs ar threads, kurš pagaidām ir aizpildīts ar nullēm
+    #tiek izveidots masīvs ar threads, kurš pagaidām ir aizpildīts ar nullēm. tas tiek izmantots, lai 
+    # atrastu thread, kurš ātrāk beigs darbu
     threads = []
     for a in range(n):
         threads.append(0)
+    #masīvs, lai saglabātu katra thread darba sākumu
     beginTime = []
     for b in range(m):
         beginTime.append(0)
+    #masīvs, lai saglabātu katra thread indeksu
     threadIndex = []
     for c in range(m):
         threadIndex.append(0)
     
     k=0
     while k<m:
-        #tiek paņemts pirmais threads elements un tiek salīdzināts ar citiem, lai atrastu thread ar mazāko vērtību
+        #tiek paņemts pirmais threads elements un tiek salīdzināts ar citiem, lai atrastu thread ar mazāko laiku
         minTime = threads[0]
         index = 0
         l=1
@@ -26,13 +29,14 @@ def parallel_processing(n, m, data):
                 minTime = threads[l]
                 index = l
             l+=1
-        
+        #beginTime masīvā tiek saglabāta threads vērtība, kura atbilst thread darba sākumam
         beginTime[k] = threads[index]
+        #threadIndex masīvā tiek saglabāts thread indekss, kurš šobrīd veic darbu
         threadIndex[k] = index
+        #attiecīgā thread laiks tiek palielināts par tik, cik ilgs ir pildāmais darbs
         threads[index] += data[k]
-        #output.append((index,begin))
         k+=1
-    
+    #tiek aizpildīts output masīvs ar thread indeksiem un darbu izpildes laikiem attiecīgā secībā
     for i in range(m):
         output.append((threadIndex[i],beginTime[i]))
     return output
